9b00c17c0d0db397374b30f1b9183a6581502f6b
karisafa279 <ksanchezf279@ulacit.ed.cr>
2017-10-11 13:27:55 -0600

    Update PingPong

--- PingPong.py
+++ PingPong.py
@@ -11,8 +11,11 @@ while (bola < 3):
 
 	b=0
 	c=0
+	string = ""
 	alefil1 = 0
 	alecol2 = 0 
+	flagp = False  #bandera que se usa para saber que ninguno de los dos jugadores a ganado, se vuelve true cuando se toca un borde.
+	flagb = False  #bandera que se usa como condicion para encontrar la bola.
 	
 	
 	print("Juego numero "+str(bola+1))
@@ -34,18 +37,40 @@ while (bola < 3):
 	ale2= random.randrange(3,6,1)   #columna de la 3 a la 6 que aparezca la bola
 	matriz[ale1][ale2]='O'	
 	
+	#while (flagp == False):
+	
 	
 	for j in range(10):            # paleta en la fila 1
 			if(matriz[1][j]=='X'):
 				b = j		       # b = posicion de la primer x en la paleta 1
-			
+				
 	for j in range(10):            # paleta en la fila 30
 			if(matriz[30][j]=='X'): 
 				c = j			   # c = posicion de la primer x en la paleta 2
 
+		#for i in range(32):
+		#	for j in range(10):
+				#El siguiente codigo es para mover la bola una vez encontrada 
+		#		if(matriz[i][j]=='O' and flagb == False ):
+		#			matriz[i][j]=''#Elimina la bola de la posicion actual
+			
+				#	if(matriz[i+1][j+1]=='|'
+				
+	
+
+	#El siguiente codigo es usado para concatenar la matriz y mostrarla sin corchetes 
+	for i in range(32):
+		for j in range(10):
+			string+=str(matriz[i][j])+'\t'
+		
+		print (string)
+		string=""
+
+	
 	bola = bola+1	
 
-if(jugador1 > jugador2):
-	print("Jugador uno Gano")
-else:
-	print("Jugador dos Gano")
+
+#if(jugador1 > jugador2):
+#	print("Jugador uno Gano")
+#else:
+#	print("Jugador dos Gano")
